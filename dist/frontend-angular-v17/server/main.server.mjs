9ea8aafc66d61697f636851b601d46cd6f479239
dmarquezsv <devsmarquez@gmail.com>
2024-07-17 14:56:55 -0600

    Nueva ruta home 2

--- dist/frontend-angular-v17/server/main.server.mjs
+++ dist/frontend-angular-v17/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-YU22DNPM.mjs";import"./chunk-FOAWLITR.mjs";import"./chunk-WB73GXPL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-Y2QUH2U7.mjs";import"./chunk-FOAWLITR.mjs";import"./chunk-WB73GXPL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
