30dfff088e0f5eb8430cecabaebfa63eddc8eb65
dmarquezsv <devsmarquez@gmail.com>
2024-07-17 14:55:59 -0600

    Nueva ruta home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: UserComponent },
+];
+
